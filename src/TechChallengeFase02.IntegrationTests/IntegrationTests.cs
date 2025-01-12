b3eb9d1833dd0c67c256394474d1cf1685f99b7c
Larissa Barros <laarissa_barros@hotmail.com>
2025-01-11 22:08:28 -0300

    Add testes endpoints

--- src/TechChallengeFase02.IntegrationTests/IntegrationTests.cs
+++ src/TechChallengeFase02.IntegrationTests/IntegrationTests.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using System.Net;
+using System.Text;
 
 namespace TechChallengeFase02.IntegrationTests
 {
@@ -15,13 +18,137 @@ namespace TechChallengeFase02.IntegrationTests
         [Fact]
         public async Task Test_GetContacts_ReturnsSuccess()
         {
-            var response = await _client.GetAsync("/api/contatos/buscar-todos");  // URL da sua API
+            var response = await _client.GetAsync("/api/contatos/buscar-todos");  
             response.EnsureSuccessStatusCode();  // Valida se o status é 200 OK
 
             var content = await response.Content.ReadAsStringAsync();
             Assert.NotEmpty(content); 
 
             Console.WriteLine("Test_GetContacts_ReturnsSuccess passou com sucesso!");
+        }
+
+        [Fact]
+        public async Task Test_GetContactsByDDD_ReturnsSuccess()
+        {
+            var response = await _client.GetAsync("/api/contatos/buscar-ddd/11");  
+            response.EnsureSuccessStatusCode();  // Valida se o status é 200 OK
+
+            var content = await response.Content.ReadAsStringAsync();
+            Assert.NotEmpty(content);
+
+            Console.WriteLine("Test_GetContactsByDDD_ReturnsSuccess passou com sucesso!");
+        }
+
+        [Fact]
+        public async Task Test_CreateContact_ReturnsSuccess()
+        {
+            var jsonData = JsonConvert.SerializeObject(new 
+            { 
+                Nome = "Nome Usuario",
+                Telefone = "78389945578",
+                Email = "user@example.com"
+            });
+            HttpContent httpContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var response = await _client.PostAsync("/api/contatos/criar-contato", httpContent); 
+            response.EnsureSuccessStatusCode();  // Valida se o status é sucesso
+
+            var content = await response.Content.ReadAsStringAsync();
+            Assert.NotEmpty(content);
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+
+            Console.WriteLine("Test_CreateContact_ReturnsSuccess passou com sucesso!");
+        }
+
+        [Fact]
+        public async Task Test_CreateContact_ReturnsBadRequest()
+        {
+            var jsonData = JsonConvert.SerializeObject(new
+            {
+                Nome = "Nome Usuario",
+                Telefone = "783899",
+                Email = "user@example.com"
+            });
+            HttpContent httpContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var response = await _client.PostAsync("/api/contatos/criar-contato", httpContent);
+            
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+
+            Console.WriteLine("Test_CreateContact_ReturnsBadRequest passou com sucesso!");
+        }
+
+        [Fact]
+        public async Task Test_UpdateContact_ReturnsSuccess()
+        {
+            var jsonData = JsonConvert.SerializeObject(new
+            {
+                Nome = "Nome Usuario",
+                Telefone = "78389945579",
+                Email = "user@example.com"
+            });
+            HttpContent httpContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var response = await _client.PutAsync("/api/contatos/atualizar-contato/1", httpContent);
+            response.EnsureSuccessStatusCode();  // Valida se o status é 200 OK
+
+            var content = await response.Content.ReadAsStringAsync();
+            Assert.NotEmpty(content);
+
+            Console.WriteLine("Test_UpdateContact_ReturnsSuccess passou com sucesso!");
+        }
+
+        [Fact]
+        public async Task Test_UpdateContact_ReturnsNotFound()
+        {
+            var jsonData = JsonConvert.SerializeObject(new
+            {
+                Nome = "Nome Usuario",
+                Telefone = "78389945579",
+                Email = "user@example.com"
+            });
+            HttpContent httpContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var response = await _client.PutAsync("/api/contatos/atualizar-contato/0", httpContent);
+
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+
+            Console.WriteLine("Test_UpdateContact_ReturnsNotFound passou com sucesso!");
+        }
+
+        [Fact]
+        public async Task Test_UpdateContact_ReturnsBadRequest()
+        {
+            var jsonData = JsonConvert.SerializeObject(new
+            {
+                Nome = "Nome Usuario",
+                Telefone = "783899",
+                Email = "user@example.com"
+            });
+            HttpContent httpContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var response = await _client.PutAsync("/api/contatos/atualizar-contato/1", httpContent);
+
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+
+            Console.WriteLine("Test_UpdateContact_ReturnsBadRequest passou com sucesso!");
+        }
+
+        [Fact]
+        public async Task Test_DeleteContact_ReturnsSuccess()
+        {
+            var response = await _client.DeleteAsync("/api/contatos/excluir-contato/1");
+            response.EnsureSuccessStatusCode();  // Valida se o status é 200 OK
+
+            var content = await response.Content.ReadAsStringAsync();
+            Assert.NotEmpty(content);
+
+            Console.WriteLine("Test_DeleteContact_ReturnsSuccess passou com sucesso!");
+        }
+
+        [Fact]
+        public async Task Test_DeleteContact_ReturnsNotFound()
+        {
+            var response = await _client.DeleteAsync("/api/contatos/excluir-contato/0");
+
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+
+            Console.WriteLine("Test_DeleteContact_ReturnsNotFound passou com sucesso!");
         }
     }
 }
